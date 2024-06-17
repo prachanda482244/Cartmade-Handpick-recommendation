bcd1dd9de38e86f888504752fa4436de5c78bac1
=Prachanda Rana <=>
2024-06-17 15:35:41 +0545

    make the another search field

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,37 +1,3 @@
-// import { MongoClient, ObjectId as BSONObjectId } from "mongodb";
-
-// // Define MongoDB connection string
-// const connectionString: string | any = process.env.CONNECTION_STRING;
-
-// // Check if the connection string is provided
-// if (!connectionString) {
-//   throw new Error(
-//     "No connection string provided. Please set the CONNECTION_STRING environment variable.",
-//   );
-// }
-
-// // Declare MongoDB client
-// let mongodb: MongoClient;
-
-// // Connect to MongoDB
-// async function connectToMongoDB() {
-//   try {
-//     mongodb = new MongoClient(connectionString);
-//     await mongodb.connect();
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     throw error;
-//   }
-// }
-
-// // Call connect function in development
-// if (process.env.NODE_ENV !== "production") {
-//   connectToMongoDB();
-// }
-
-// export { mongodb, BSONObjectId as ObjectId };
-
 import { PrismaClient } from "@prisma/client";
 
 declare global {
--- app/routes/app.dashboard.tsx
+++ app/routes/app.dashboard.tsx
@@ -1,6 +1,5 @@
 import { LoaderFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { DeleteIcon } from "@shopify/polaris-icons";
-
 import { useLoaderData } from "@remix-run/react";
 import {
   Box,
@@ -10,20 +9,22 @@ import {
   FormLayout,
   IndexTable,
   Layout,
+  LegacyCard,
   Page,
+  Scrollable,
   Spinner,
   TextField,
   useIndexResourceState,
 } from "@shopify/polaris";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { apiVersion, authenticate } from "~/shopify.server";
 import { pageInfomation } from "~/config/typeConfig";
-
+import lodash from "lodash";
 export const query = `
 {
   products(first:10) {
-   pageInfo {
+    pageInfo {
       hasPreviousPage
       hasNextPage
       startCursor
@@ -34,7 +35,7 @@ export const query = `
         id
         title
         createdAt
-     		vendor
+        vendor
         featuredImage {
           url
         }
@@ -48,7 +49,6 @@ export const query = `
     }
   }
 }
-
 `;
 
 export const loader: LoaderFunction = async ({
@@ -90,10 +90,12 @@ const Dashboard = () => {
     hasPreviousPage: false,
   });
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<any>([]);
+  const [searchProducts, setSearchProducts] = useState<any>([]);
   const { pageInfo, edges } = loaderData;
 
   const [inputValue, setInputValue] = useState<string>("");
+  const [insideInputValue, setInsideInputValue] = useState<string>("");
   const [selectedProductId, setSelectedProductId] = useState<string | null>(
     null,
   );
@@ -129,41 +131,44 @@ const Dashboard = () => {
     setProducts(edges);
   };
 
-  const handleSubmit = async () => {
-    console.log("api call");
+  const handleSubmit = async (value: string) => {
     try {
       setIsLoading(true);
       let response = await fetch(
-        `/api/search?firstLastValue=first&searchQuery=${inputValue}`,
+        `/api/search?firstLastValue=first&searchQuery=${value}`,
       );
       const { data } = await response.json();
       const { products } = data;
       const { pageInfo, edges } = products;
       setPageInformation(pageInfo);
-      setProducts(edges);
+      insideInputValue === inputValue
+        ? setProducts(edges)
+        : setSearchProducts(edges);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
+  // const debouncedSearch = useCallback(lodash.debounce(handleSubmit, 500), [
+  //   inputValue,
+  // ]);
+
   const resourceIDResolver = (product: any) => product.node.id;
 
   const { selectedResources, allResourcesSelected } = useIndexResourceState(
     products,
-    { resourceIDResolver },
+    {
+      resourceIDResolver,
+    },
   );
 
   const promotedBulkActions = [
-    // {
-    //   content: "Capture payments",
-    //   onAction: () => console.log("Todo: implement payment capture"),
-    // },
     {
       title: "Add Selected Products",
       actions: [
         {
-          content: "Add  Products",
+          content: "Add Products",
           onAction: () => console.log("Todo: implement adding customers"),
         },
         {
@@ -191,33 +196,51 @@ const Dashboard = () => {
       onAction: () => console.log("Todo: implement bulk remove tags"),
     },
     {
-      content: "Sort assending",
+      content: "Sort ascending",
       onAction: () => console.log("Todo: implement bulk remove tags"),
     },
     {
-      content: "Sort Desending",
+      content: "Sort descending",
       onAction: () => console.log("Todo: implement bulk remove tags"),
     },
   ];
 
-  const rowMarkup = products.map((product: any, index) => {
+  const handleToggleAccordion = (productId: string) => {
+    console.log(productId, "Product id");
+    setSelectedProductId(selectedProductId === productId ? null : productId);
+    setIsVisible(false);
+  };
+
+  const handleSelectionChange = (selected: string[] | any) => {
+    setSelectedProductId(selected.length > 0 ? selected[0] : null);
+  };
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const handleAddRelatedProduct = () => {
+    setSearchProducts(products);
+    setIsVisible(true);
+  };
+
+  const rowMarkup = products.map((product: any, index: number) => {
     const { id, priceRange, title, featuredImage, vendor, createdAt } =
       product.node;
+    const isExpanded = selectedProductId === id;
+
     return (
       <>
         <IndexTable.Row
           key={id}
           id={id}
-          selected={id === selectedProductId}
-          onClick={() => setSelectedProductId(id)}
+          selected={isExpanded}
+          onClick={() => handleToggleAccordion(id)}
           position={index}
         >
-          <IndexTable.Cell>{title}</IndexTable.Cell>
+          <IndexTable.Cell className="border">{title}</IndexTable.Cell>
           <IndexTable.Cell>{createdAt}</IndexTable.Cell>
           <IndexTable.Cell>
             <img
               src={featuredImage?.url}
-              alt="Products"
+              alt="Product"
               height={40}
               width={60}
             />
@@ -227,28 +250,87 @@ const Dashboard = () => {
           </IndexTable.Cell>
           <IndexTable.Cell>{vendor}</IndexTable.Cell>
         </IndexTable.Row>
-        <div>
-          {selectedProductId === id ? (
-            <div className="text-2xl font-bold">bye bye</div>
-          ) : null}
-        </div>
+
+        {isExpanded && (
+          <div className="border w-[50vw]">
+            <div>{title}</div>
+            <div>
+              {!isVisible && (
+                <button
+                  onClick={handleAddRelatedProduct}
+                  className="py-2 px-4 bg-green-400 text-slate-200"
+                >
+                  Addd related product
+                </button>
+              )}
+
+              {isVisible && (
+                <div className=" h-96 flex flex-col gap-3 mt-2 w-full z-[9999] text-whiten">
+                  <div className="flex items-center gap-2">
+                    <Form
+                      noValidate
+                      onSubmit={() => handleSubmit(insideInputValue)}
+                    >
+                      <FormLayout>
+                        <div className="flex items-center gap-2">
+                          <TextField
+                            label=""
+                            value={insideInputValue}
+                            onChange={(value) => setInsideInputValue(value)}
+                            placeholder="Search "
+                            type="search"
+                            autoComplete="off"
+                          />
+
+                          <Button variant="secondary" submit>
+                            Search
+                          </Button>
+                        </div>
+                      </FormLayout>
+                    </Form>
+                  </div>
+                  <Scrollable shadow style={{ height: "350px" }}>
+                    {searchProducts?.map((product: any, index: number) => (
+                      <div
+                        onClick={() => handleClick(product.node.id)}
+                        key={product.node.id}
+                        className="flex border p-3 hover:bg-slate-300 cursor-pointer"
+                      >
+                        {isLoading ? (
+                          "Loading"
+                        ) : (
+                          <>
+                            <div>{index + 1}</div>
+                            {product.node.title}
+                            {product.node.vendor}
+                            <img
+                              src={product.node.featuredImage?.url}
+                              alt="product"
+                              height={20}
+                              width={20}
+                            />
+                          </>
+                        )}
+                      </div>
+                    ))}
+                  </Scrollable>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </>
     );
   });
 
-  const handleChange = async (value: string) => {
+  const handleClick = (v: any) => {
+    console.log(v);
+  };
+
+  const handleChange = (value: string) => {
     setInputValue(value);
   };
 
-  const handleSelectionChange = (selected: string[] | any) => {
-    if (selected.length === 0) {
-      setSelectedProductId(null);
-      return;
-    }
-    setSelectedProductId(selected[0]);
-  };
-  console.log(selectedProductId);
-  // console.log(products);
   return (
     <Page>
       <Layout>
@@ -257,13 +339,13 @@ const Dashboard = () => {
         </Layout.Section>
 
         <Layout.Section>
-          <Form noValidate onSubmit={handleSubmit}>
+          <Form noValidate onSubmit={() => handleSubmit(inputValue)}>
             <FormLayout>
               <div className="flex items-center gap-2">
                 <TextField
-                  label
+                  label=""
                   value={inputValue}
-                  onChange={(value) => handleChange(value)}
+                  onChange={handleChange}
                   placeholder="Search products"
                   type="search"
                   autoComplete="off"
@@ -276,11 +358,13 @@ const Dashboard = () => {
             </FormLayout>
           </Form>
         </Layout.Section>
+
         <Layout.Section>
           <Card>
             <h1>All products</h1>
           </Card>
         </Layout.Section>
+
         <Layout.Section>
           <Card>
             <Box paddingBlockEnd="400">
@@ -302,19 +386,15 @@ const Dashboard = () => {
                 pagination={{
                   hasNext: pageInformation.hasNextPage,
                   hasPrevious: pageInformation.hasPreviousPage,
-                  onNext: () => {
-                    handleNextPagination();
-                  },
-                  onPrevious: () => {
-                    handlePrevPagination();
-                  },
+                  onNext: handleNextPagination,
+                  onPrevious: handlePrevPagination,
                   label: "Total products",
                 }}
               >
-                {isLoading ? (
+                {inputValue && isLoading ? (
                   <div className="text-2xl flex items-center justify-center h-40 w-[55vw] mx-auto">
                     <Spinner
-                      accessibilityLabel="Spinner example"
+                      accessibilityLabel="Loading spinner"
                       size="large"
                     />
                   </div>
